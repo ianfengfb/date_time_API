52a9e0f56e901503714f6e996bf0a0cd2a72ee19
ianfengfb <ianfengfb@gmail.com>
2023-03-16 02:02:59 +1100

    add 2 test methods

--- tests/Unit/CalculatorTest.php
+++ tests/Unit/CalculatorTest.php
@@ -2,15 +2,26 @@
 
 namespace Tests\Unit;
 
+use App\Calculator\DateTimeCalculator;
 use PHPUnit\Framework\TestCase;
 
 class CalculatorTest extends TestCase
 {
     /**
-     * A basic unit test example.
+     * Test days difference calculator, input same two days
      */
-    public function test_example(): void
+    public function test_days_diff_calculator_same_two_days_return_zero_diff(): void
     {
-        $this->assertTrue(true);
+        $days_diff = (new DateTimeCalculator())->days_diff_calculator('2023-03-15T10:15', '2023-03-15T11:15');
+        $this->assertEquals(0, $days_diff);
+    }
+
+    /**
+     * Test weeks difference calculator, input two days in the same week
+     */
+    public function test_weeks_diff_calculator_two_days_in_the_same_week_return_zero_diff(): void
+    {
+        $weeks_diff = (new DateTimeCalculator())->weeks_diff_calculator('2023-03-15T10:15', '2023-03-18T11:15');
+        $this->assertEquals(0, $weeks_diff);
     }
 }
