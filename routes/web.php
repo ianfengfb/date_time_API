52f3754bb043b24320d93da411bee6d4cbb47b60
ianfengfb <ianfengfb@gmail.com>
2023-03-16 01:47:00 +1100

    create page controller and page routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*
+|--------------------------------------------------------------------------
+| Page Routes
+|--------------------------------------------------------------------------
+||
+| Add by Ian
+| On 15 Mar 2023
+|
+*/
+
+// Display the index page
+Route::get('/', [PageController::class, 'index']);
